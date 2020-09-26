1be40d0422a072bc3b91be1f056a586c61a1e636
wanchana.e <wanchana.e@kbtg.tech>
2020-09-26 11:29:40 +0700

    refactoring logger middleware

--- homatic/logger/logger.go
+++ homatic/logger/logger.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 )
 
+type LogKey string
+
+const key LogKey = "logger"
+
 func MiddleWare(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		l := zap.NewExample()
 		l = l.With(zap.Namespace("homatic"), zap.String("I'm", "gopher"))
-		c := context.WithValue(r.Context(), "logger", l)
+		c := context.WithValue(r.Context(), key, l)
 		newR := r.WithContext(c)
 		next.ServeHTTP(w, newR)
 	})
 }
 
 func GetLog(ctx context.Context) *zap.Logger {
-	value := ctx.Value("logger")
+	value := ctx.Value(key)
 	if value == nil {
 		return zap.NewExample()
 	}
